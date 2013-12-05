813a52782527fa0868b3979729a95c93e966b8d6
molst <molst@yahoo.com>
2013-12-05 22:13:15 +0100

    updated project info

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject treq "0.1-SNAPSHOT"
+(defproject treq "0.2-SNAPSHOT"
   :description "Library for converting edn trees of requests into function calls."
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
+  :url "https://github.com/molst/treq"
+  :scm {:name "git" :url "https://github.com/molst/treq"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.1.7"]
-                 [torpo "0.1-SNAPSHOT"]
-                 [shaky "0.1-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
-  :scm {:name "git" :url "https://github.com/molst/treq"})
+                 [torpo "0.2-SNAPSHOT"]
+                 [shaky "0.2-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
