78571bb31ad30a68c0d378afb26d1cab6987b890
molst <molst@yahoo.com>
2013-12-05 20:49:37 +0100

    no snapshot

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject treq "0.1-SNAPSHOT"
+(defproject treq "0.1"
   :description "Library for converting edn trees of requests into function calls."
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.1.7"]
-                 [torpo "0.1-SNAPSHOT"]
-                 [shaky "0.1-SNAPSHOT"]]
+                 [torpo "0.1"]
+                 [shaky "0.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
